a9948b2528656a67cf5eabaf12b5e51419a0dd6f
Galina Netishina <netishinagalina@yandex.ru>
2024-06-06 16:00:52 +0300

    readme and small rewiew

--- src/js/__tests__/http.test.js
+++ src/js/__tests__/http.test.js
@@ -1,14 +1,24 @@
 import getLevel from '../getLevel';
 import httpGet from '../http';
+import nock from "nock";
 jest.mock('../http');
 
 beforeEach(() => {
     jest.resetAllMocks();
 });
-test('get level', () => {
+test('http get have been called with correct argument', () => {
     httpGet.mockReturnValue(JSON.stringify({}));
     getLevel(1);
     expect(httpGet).toHaveBeenCalledWith('https://server/user/1');
     getLevel(2);
     expect(httpGet).toHaveBeenCalledWith('https://server/user/2');
 })
+
+// describe('some  test', function(){
+//     it('success request', ()=>{
+//         nock('https://server/user')
+//         .get('/t')
+//         .reply(200, {data: {level: 1}});
+//         expect
+//     })
+// })
